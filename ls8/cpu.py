d7b57f262708ad73916127e47e55dee0a586fc1c
Jacob Prudent <jake.prudent@gmail.com>
2020-06-18 16:07:25 -0700

    MVP!!!!

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,8 +8,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        # Registers
         self.reg = [0]*8
+        # program counter
         self.pc = 0
+        # flag
+        self.fl = 0
+        # self.e_fl = 0
+        # self.l_fl = 0
+        # self.g_fl = 0
         self.ram = [0]*256
         self.stop = False
 
@@ -64,16 +71,46 @@ class CPU:
             self.reg[reg_a] *= self.reg[reg_b]
         elif op == "DIV":
             self.reg[reg_a] /= self.reg[reg_b]
+        elif op == "DEC":
+            self.reg[reg_a] -= 1
+        elif op == "INC":
+            self.reg[reg_a] += 1
+        elif op == "CMP":
+            # Flag example
+            # `AA B C DDDD`
+            #  00 0 0 0LGE
+            #  10 1 0 0111
+            reg_a_value = self.reg[reg_a]
+            reg_b_value = self.reg[reg_b]
+            if reg_a_value > reg_b_value:
+                self.fl = 0b00000010
+            elif reg_a_value == reg_b_value:
+                self.fl = 0b00000001
+            elif reg_a_value < reg_b_value:
+                self.fl = 0b00000100
+        elif op == "MOD":
+            self.reg[reg_a]
+
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         # self.fl,
+    #         # self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
+    def trace(self, LABEL=str()):
+
+        print(f"{LABEL} TRACE --> PC: %02i | RAM: %03i %03i %03i | Register: " % (
             self.pc,
             # self.fl,
             # self.ie,
@@ -81,6 +118,13 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
+        for i in range(8):
+            print(" %02i" % self.reg[i], end='')
+        print(" | Stack:", end='')
+
+        for i in range(240, 244):
+            print(" %02i" % self.ram_read(i), end='')
+        print()
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
@@ -90,78 +134,114 @@ class CPU:
     # ALU Functions
 
     def ADD(self):
+        """Add the value in two registers and store the result in registerA."""
         reg_a = self.ram_read(self.pc+1)
         reg_b = self.ram_read(self.pc+2)
         self.alu('ADD', reg_a, reg_b)
         self.pc += 3
 
     def SUB(self):
+        """Subtract the value in the second register from the first, storing the result in registerA."""
         reg_a = self.ram_read(self.pc+1)
         reg_b = self.ram_read(self.pc+2)
         self.alu('SUB', reg_a, reg_b)
         self.pc += 3
 
     def MUL(self):
-        # Eventually I will want to hook this up to the ALU
-        # I will want to perform the operations in the ALU
-        # using only && and ||. I would want ADD to use these
-        # and multiply to preform this operation over and overagain
-
-        # For MVP I will just multiply the two together here
+        """Multiply the values in two registers together and store the result in registerA."""
         reg_a = self.ram_read(self.pc+1)
         reg_b = self.ram_read(self.pc+2)
         self.alu('MUL', reg_a, reg_b)
         self.pc += 3
 
     def DIV(self):
+        """Divide the value in the first register
+        by the value in the second,
+        storing the result in registerA.
+        If the value in the second register is 0,
+        the system should print an error message and halt."""
         reg_a = self.ram_read(self.pc+1)
         reg_b = self.ram_read(self.pc+2)
         self.alu('DIV', reg_a, reg_b)
         self.pc += 3
 
     def MOD(self):
+        """Divide the value in the first register by the value in the second, 
+        storing the remainder of the result in registerA.
+        If the value in the second register is 0, 
+        the system should print an error message and halt."""
         print("MOD not implemented yet")
-        pass
+        reg_a = self.ram_read(self.pc+1)
+        reg_b = self.ram_read(self.pc+1)
+        self.alu("MOD", reg_a, reg_b)
+        self.pc += 2
 
     def INC(self):
-        print("INC not implemented yet")
-        pass
+        """Increment (add 1 to) the value in the given register."""
+        reg_a = self.ram_read(self.pc+1)
+        reg_b = None
+        self.alu("INC", reg_a, reg_b)
+        self.pc += 2
 
     def DEC(self):
-        print("DEC not implemented yet")
-        pass
+        """Decrement (subtract 1 from) the value in the given register."""
+        reg_a = self.ram_read(self.pc+1)
+        reg_b = None
+        self.alu("DEC", reg_a, reg_b)
+        self.pc += 2
 
     def CMP(self):
-        print("CMP not implemented yet")
-        pass
+        # print("Compare Ran")
+        """
+        Compare the values in two registers.
+        If they are equal, set the Equal E flag to 1, otherwise set it to 0.
+        If registerA is less than registerB, set the Less-than L flag to 1, otherwise set it to 0.
+        If registerA is greater than registerB, set the Greater-than G flag to 1, otherwise set it to 0.
+        """
+        reg_a = self.ram_read(self.pc+1)
+        reg_b = self.ram_read(self.pc+2)
+        self.alu("CMP", reg_a, reg_b)
+        self.pc += 3
 
     def AND(self):
         print("AND not implemented yet")
         pass
 
     def NOT(self):
+        """Perform a bitwise-NOT on the value in a register,
+        storing the result in the register."""
         print("NOT not implemented yet")
         pass
 
     def OR(self):
+        """Perform a bitwise-OR between the values in registerA and registerB,
+        storing the result in registerA."""
         print("OR not implemented yet")
         pass
 
     def XOR(self):
+        """Perform a bitwise-XOR between the values in registerA and registerB,
+        storing the result in registerA."""
         print("XOR not implemented yet")
         pass
 
     def SHL(self):
+        """Shift the value in registerA left by the number
+        of bits specified in registerB,
+        filling the low bits with 0."""
         print("SHL not implemented yet")
         pass
 
     def SHR(self):
+        """Shift the value in registerA right by the number of bits specified in registerB,
+        filling the high bits with 0."""
         print("SHR not implemented yet")
         pass
 
     # PC Mutator
 
     def CALL(self):
+        print("Call Ran")
         """1. The address of the ***instruction*** _directly after_ `CALL`
         is pushed onto the stack.
         This allows us to return to where we left off when the subroutine finishes executing.
@@ -204,32 +284,77 @@ class CPU:
         pass
 
     def JMP(self):
-        print("JMP not implemented yet")
-        pass
+        # print("JMP Ran")
+        """Jump to the address stored in the given register.
+        Set the PC to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        self.pc = self.reg[location]
 
     def JEQ(self):
-        print("JEQ not implemented yet")
-        pass
+        # print("JEQ Ran")
+        """JEQ register
+        If equal flag is set (true), jump to the address stored in the given register. """
+        # if reg_a_value > reg_b_value:
+        #         self.fl = 0b00000010
+        #     elif reg_a_value == reg_b_value:
+        #         self.fl = 0b00000001
+        #     elif reg_a_value < reg_b_value:
+        #         self.fl = 0b00000100
+        location = self.ram_read(self.pc + 1)
+        if self.fl == 0b00000001:
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     def JNE(self):
-        print("JNE not implemented yet")
-        pass
+        # print("JNE Ran")
+        """If E flag is clear (false, 0),
+        jump to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        if self.fl != 0b00000001:
+            # print(self.pc+1)
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     def JGT(self):
-        print("JGT not implemented yet")
-        pass
+        # print("JGT Ran")
+        """If greater-than flag is set (true),
+        jump to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        if self.fl == 0b00000010:
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     def JLT(self):
-        print("JLT not implemented yet")
-        pass
+        # print("JLT Ran")
+        """If less-than flag is set (true), jump to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        if self.fl == 0b00000100:
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     def JLE(self):
-        print("JLE not implemented yet")
-        pass
+        # print("JLE Ran")
+        """If less-than flag or equal flag is set (true),
+        jump to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        if self.fl == 0b00000100 or self.fl == 0b00000001:
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     def JGE(self):
-        print("JGE not implemented yet")
-        pass
+        # print("JGE Ran")
+        """If greater-than flag or equal flag is set (true),
+        jump to the address stored in the given register."""
+        location = self.ram_read(self.pc + 1)
+        if self.fl == 0b00000010 or self.fl == 0b00000001:
+            self.pc = self.reg[location]
+        else:
+            self.pc += 2
 
     # Other
 
@@ -250,6 +375,7 @@ class CPU:
         LDI register immediate
         Set the value of a register to an integer.
         """
+        # print("LDI Ran")
         # Register number/ Memory Location
         operand_a = self.ram_read(self.pc + 1)
         # Value to be stored
@@ -258,16 +384,18 @@ class CPU:
         self.pc += 3
 
     def LD(self):
+        """Loads registerA with the value at the memory address stored in registerB."""
         print("LD not implemented yet")
         pass
 
     def ST(self):
+        """Store value in registerB in the address stored in registerA."""
         print("ST not implemented yet")
         pass
 
     def PUSH(self):
         """Places something on the stack, stored in Ram"""
-        print("Push ran")
+        # print("Push ran")
         # Decrement the sp (R7)
         self.reg[7] -= 1
         # Copy the value given to the address pointed by SP
@@ -280,6 +408,7 @@ class CPU:
         self.pc += 2
 
     def POP(self):
+        # print("POP ran")
         # Copy the value from memory at the address pointed to by SP
         address = self.reg[7]
         value = self.ram[address]
@@ -302,6 +431,8 @@ class CPU:
         self.pc += 2
 
     def PRA(self):
+        """Print alpha character value stored in the given register.
+        Print to the console the ASCII character corresponding to the value in the register."""
         print("PRA not yet implimented")
         pass
 
@@ -369,7 +500,7 @@ class CPU:
             if IR in branchTable:
                 IR = self.ram_read(self.pc)  # Instruction register
                 # print(f"IR is {IR}")
-                self.trace()
+                # self.trace()
                 branchTable[IR]()
             else:
                 print(
